f4e96b2d3add13f70e14bdd97d044423a39ef70b
yovitaHandayiani <yovita.handayiani@sg-dsa.com>
2026-09-15 13:46:16 +0700

    fix: EDTSButton
    - rename "label" to "text"

--- EDTS_DS_SwiftUI/Views/Components/General/Button/Default/EDTSButton.swift
+++ EDTS_DS_SwiftUI/Views/Components/General/Button/Default/EDTSButton.swift
@@ -32,11 +32,11 @@ public struct EDTSButton: View {
     public var btnSize: BtnSize
     public var btnState: BtnState
 
-    public let label: String?
-    public let labelAttributed: AttributedString?
-    public var labelColor: Color?
-    public var labelDangerColor: Color?
-    public var labelDisabledColor: Color?
+    public let text: String?
+    public let textAttributed: AttributedString?
+    public var textColor: Color?
+    public var textDangerColor: Color?
+    public var textDisabledColor: Color?
     
     public var fontStyle: Font?
     public var fontName: String
@@ -94,11 +94,11 @@ public struct EDTSButton: View {
         btnType: BtnType = .primary,
         btnSize: BtnSize = .large,
         btnState: BtnState = .default,
-        label: String? = "Button",
-        labelAttributed: AttributedString? = nil,
-        labelColor: Color? = nil,
-        labelDangerColor: Color? = nil,
-        labelDisabledColor: Color? = nil,
+        text: String? = "Button",
+        textAttributed: AttributedString? = nil,
+        textColor: Color? = nil,
+        textDangerColor: Color? = nil,
+        textDisabledColor: Color? = nil,
         fontStyle: Font? = nil,
         fontName: String = "",
         fontSize: CGFloat = .zero,
@@ -140,11 +140,11 @@ public struct EDTSButton: View {
         self.btnType = btnType
         self.btnSize = btnSize
         self.btnState = btnState
-        self.label = labelAttributed == nil ? (label ?? "Button") : nil
-        self.labelAttributed = labelAttributed
-        self.labelColor = labelColor
-        self.labelDangerColor = labelDangerColor
-        self.labelDisabledColor = labelDisabledColor
+        self.text = textAttributed == nil ? text : nil
+        self.textAttributed = textAttributed
+        self.textColor = textColor
+        self.textDangerColor = textDangerColor
+        self.textDisabledColor = textDisabledColor
         self.fontStyle = fontStyle
         self.fontName = fontName
         self.fontSize = fontSize
@@ -200,7 +200,7 @@ public struct EDTSButton: View {
     private struct ResolvedValues {
         var tempIconTintColorLeading: Color?
         var tempIconTintColorTrailing: Color?
-        var tempLabelColor: Color?
+        var tempTextColor: Color?
         var tempBgColor: Color?
         var tempRippleColor: Color?
         var tempBorderColor: Color?
@@ -273,14 +273,14 @@ public struct EDTSButton: View {
             }
             
             Group {
-                if let labelAttributed {
-                    Text(labelAttributed)
+                if let textAttributed {
+                    Text(textAttributed)
                 } else {
-                    Text(label ?? "Button")
+                    Text(text ?? "Button")
                 }
             }
             .edtsFont(setupFontStyle, custom: customFont)
-            .foregroundColor(values.tempLabelColor)
+            .foregroundColor(values.tempTextColor)
             .multilineTextAlignment(.center)
             
             if let iconTrailing {
@@ -404,10 +404,10 @@ public struct EDTSButton: View {
             break
             
         case .secondary, .tertiary:
-            if labelColor != nil {
-                values.tempIconTintColorLeading = iconTintColorLeading == nil ? values.tempLabelColor : values.tempIconTintColorLeading
-                values.tempIconTintColorTrailing = iconTintColorTrailing == nil ? values.tempLabelColor : values.tempIconTintColorTrailing
-                values.tempBorderColor = borderColor == nil ? values.tempLabelColor : values.tempBorderColor
+            if textColor != nil {
+                values.tempIconTintColorLeading = iconTintColorLeading == nil ? values.tempTextColor : values.tempIconTintColorLeading
+                values.tempIconTintColorTrailing = iconTintColorTrailing == nil ? values.tempTextColor : values.tempIconTintColorTrailing
+                values.tempBorderColor = borderColor == nil ? values.tempTextColor : values.tempBorderColor
             }
         }
         
@@ -418,9 +418,9 @@ public struct EDTSButton: View {
         
         if rippleColor == nil {
             if values.tempBgColor == EDTSColor.white {
-                values.tempRippleColor = values.tempLabelColor?.opacity(0.12)
+                values.tempRippleColor = values.tempTextColor?.opacity(0.12)
             } else if values.tempBgColor == .clear {
-                values.tempRippleColor = values.tempLabelColor?.opacity(0.12)
+                values.tempRippleColor = values.tempTextColor?.opacity(0.12)
             } else if values.tempBgColor != EDTSColor.white {
                 values.tempRippleColor = EDTSColor.grey70.opacity(0.12)
             }
@@ -445,14 +445,14 @@ public struct EDTSButton: View {
             }
             
             values.tempIconTintColorLeading = iconTintColorLeading ?? EDTSColor.white
-            values.tempLabelColor = labelColor ?? EDTSColor.white
+            values.tempTextColor = textColor ?? EDTSColor.white
             values.tempIconTintColorTrailing = iconTintColorTrailing ?? EDTSColor.white
             values.tempBorderWidth = borderWidth == .zero ? 0 : borderWidth
             values.tempShadowColor = shadowColor
             
         case .danger:
             values.tempIconTintColorLeading = iconDangerTintColorLeading ?? EDTSColor.white
-            values.tempLabelColor = labelDangerColor ?? EDTSColor.white
+            values.tempTextColor = textDangerColor ?? EDTSColor.white
             values.tempIconTintColorTrailing = iconDangerTintColorTrailing ?? EDTSColor.white
             values.tempBgColor = bgDangerColor ?? EDTSColor.red30
             values.tempBorderColor = borderDangerColor ?? EDTSColor.red30
@@ -463,7 +463,7 @@ public struct EDTSButton: View {
             values.tempBgColor = bgDisabledColor ?? EDTSColor.grey30
             values.tempBorderColor = borderDisabledColor ?? EDTSColor.grey30
             values.tempIconTintColorLeading = iconDisabledTintColorLeading ?? EDTSColor.white
-            values.tempLabelColor = labelDisabledColor ?? EDTSColor.white
+            values.tempTextColor = textDisabledColor ?? EDTSColor.white
             values.tempIconTintColorTrailing = iconDisabledTintColorTrailing ?? EDTSColor.white
             values.tempBorderWidth = borderWidth == .zero ? 0 : borderWidth
             values.tempShadowColor = shadowDisabledColor ?? shadowColor
@@ -475,12 +475,12 @@ public struct EDTSButton: View {
         case .default:
             if EDTSColor.theme == .poinku {
                 values.tempIconTintColorLeading = iconTintColorLeading ?? EDTSColor.blue30
-                values.tempLabelColor = labelColor ?? EDTSColor.blue30
+                values.tempTextColor = textColor ?? EDTSColor.blue30
                 values.tempIconTintColorTrailing = iconTintColorTrailing ?? EDTSColor.blue30
                 values.tempBorderColor = borderColor ?? EDTSColor.blue30
             } else {
                 values.tempIconTintColorLeading = iconTintColorLeading ?? EDTSColor.blue50
-                values.tempLabelColor = labelColor ?? EDTSColor.blue50
+                values.tempTextColor = textColor ?? EDTSColor.blue50
                 values.tempIconTintColorTrailing = iconTintColorTrailing ?? EDTSColor.blue50
                 values.tempBorderColor = borderColor ?? EDTSColor.blue50
             }
@@ -491,7 +491,7 @@ public struct EDTSButton: View {
             
         case .danger:
             values.tempIconTintColorLeading = iconDangerTintColorLeading ?? EDTSColor.red30
-            values.tempLabelColor = labelDangerColor ?? EDTSColor.red30
+            values.tempTextColor = textDangerColor ?? EDTSColor.red30
             values.tempIconTintColorTrailing = iconDangerTintColorTrailing ?? EDTSColor.red30
             values.tempBgColor = bgDangerColor ?? EDTSColor.white
             values.tempBorderColor = borderDangerColor ?? EDTSColor.red30
@@ -500,7 +500,7 @@ public struct EDTSButton: View {
             
         case .disabled:
             values.tempIconTintColorLeading = iconDisabledTintColorLeading ?? EDTSColor.grey30
-            values.tempLabelColor = labelDisabledColor ?? EDTSColor.grey30
+            values.tempTextColor = textDisabledColor ?? EDTSColor.grey30
             values.tempIconTintColorTrailing = iconDisabledTintColorTrailing ?? EDTSColor.grey30
             values.tempBorderColor = borderDisabledColor ?? EDTSColor.grey30
             values.tempBgColor = bgDisabledColor ?? EDTSColor.white
@@ -513,7 +513,7 @@ public struct EDTSButton: View {
         switch state {
         case .default:
             values.tempIconTintColorLeading = iconTintColorLeading ?? EDTSColor.grey60
-            values.tempLabelColor = labelColor ?? EDTSColor.grey60
+            values.tempTextColor = textColor ?? EDTSColor.grey60
             values.tempIconTintColorTrailing = iconTintColorTrailing ?? EDTSColor.grey60
             values.tempBgColor = bgColor ?? EDTSColor.white
             values.tempBorderColor = borderColor ?? EDTSColor.grey60
@@ -522,7 +522,7 @@ public struct EDTSButton: View {
             
         case .danger:
             values.tempIconTintColorLeading = iconDangerTintColorLeading ?? EDTSColor.red30
-            values.tempLabelColor = labelDangerColor ?? EDTSColor.red30
+            values.tempTextColor = textDangerColor ?? EDTSColor.red30
             values.tempIconTintColorTrailing = iconDangerTintColorTrailing ?? EDTSColor.red30
             values.tempBgColor = bgDangerColor ?? EDTSColor.white
             values.tempBorderColor = borderDangerColor ?? EDTSColor.grey30
@@ -531,7 +531,7 @@ public struct EDTSButton: View {
             
         case .disabled:
             values.tempIconTintColorLeading = iconDisabledTintColorLeading ?? EDTSColor.grey30
-            values.tempLabelColor = labelDisabledColor ?? EDTSColor.grey30
+            values.tempTextColor = textDisabledColor ?? EDTSColor.grey30
             values.tempIconTintColorTrailing = iconDisabledTintColorTrailing ?? EDTSColor.grey30
             values.tempBorderColor = borderDisabledColor ?? EDTSColor.grey30
             values.tempBgColor = bgDisabledColor ?? EDTSColor.white
@@ -566,20 +566,20 @@ public struct EDTSButton: View {
     struct PreviewWrapper: View {
         var body: some View {
             VStack(spacing: 12) {
-                EDTSButton(btnType: .primary, btnSize: .large, btnState: .default, label: "Primary Default") {}
-                EDTSButton(btnType: .primary, btnSize: .large, btnState: .danger, label: "Primary Danger") {}
-                EDTSButton(btnType: .primary, btnSize: .large, btnState: .disabled, label: "Primary Disabled") {}
-                EDTSButton(btnType: .secondary, btnSize: .medium, btnState: .default, label: "Secondary Default") {}
-                EDTSButton(btnType: .secondary, btnSize: .medium, btnState: .danger, label: "Secondary Danger") {}
-                EDTSButton(btnType: .secondary, btnSize: .medium, btnState: .disabled, label: "Secondary Disabled") {}
-                EDTSButton(btnType: .tertiary, btnSize: .small, btnState: .default, label: "Tertiary Default") {}
-                EDTSButton(btnType: .tertiary, btnSize: .small, btnState: .danger, label: "Tertiary Danger") {}
-                EDTSButton(btnType: .tertiary, btnSize: .small, btnState: .disabled, label: "Tertiary Disabled") {}
+                EDTSButton(btnType: .primary, btnSize: .large, btnState: .default, text: "Primary Default") {}
+                EDTSButton(btnType: .primary, btnSize: .large, btnState: .danger, text: "Primary Danger") {}
+                EDTSButton(btnType: .primary, btnSize: .large, btnState: .disabled, text: "Primary Disabled") {}
+                EDTSButton(btnType: .secondary, btnSize: .medium, btnState: .default, text: "Secondary Default") {}
+                EDTSButton(btnType: .secondary, btnSize: .medium, btnState: .danger, text: "Secondary Danger") {}
+                EDTSButton(btnType: .secondary, btnSize: .medium, btnState: .disabled, text: "Secondary Disabled") {}
+                EDTSButton(btnType: .tertiary, btnSize: .small, btnState: .default, text: "Tertiary Default") {}
+                EDTSButton(btnType: .tertiary, btnSize: .small, btnState: .danger, text: "Tertiary Danger") {}
+                EDTSButton(btnType: .tertiary, btnSize: .small, btnState: .disabled, text: "Tertiary Disabled") {}
                 EDTSButton(
                     btnType: .primary,
                     btnSize: .small,
                     btnState: .default,
-                    label: "Gradient With Icon",
+                    text: "Gradient With Icon",
                     bgColorStart: EDTSColor.skyblueLeading,
                     bgColorEnd: EDTSColor.skyblueTrailing,
                     bgColorOrientation: .vertical,
